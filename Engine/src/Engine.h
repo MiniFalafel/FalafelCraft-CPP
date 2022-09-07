ca3fcc2b2b9dc61ef38b70c170ba0c2cb769097d
MiniFalafel <eliasjsanders@gmail.com>
2022-09-06 21:01:41 -0600

    Created Engine.h for convenience

---
+++ Engine/src/Engine.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Application.h"
+
+#include "Core/Core.h"
+#include "Core/Layer.h"
+
+#include "Events/Event.h"
+#include "Events/KeyCodes.h"
+#include "Events/MouseCodes.h"
